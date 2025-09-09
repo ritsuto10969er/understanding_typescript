af7e02f43cc2167ad800995ce808b6426c639e84
ritsuto10969er <rk14oor109@gmail.com>
2025-09-10 00:50:05 +0900

    feat(ts-basics): learn function parameters with types (number, string, boolean) and type inference

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+function add(num1, num2, showResult, resultPhrase) {
+    var result = num1 + num2;
+    if (showResult) {
+        console.log(resultPhrase + result);
+    }
+    else {
+        return result;
+    }
+    return num1 + num2;
+}
+var num1;
+num1 = 1;
+var num2 = 10;
+var printResult = true;
+var resultPhrase = 'Result: ';
+add(num1, num2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
-console.log('Hello World'); 
+function add(num1: number, num2:number, showResult:boolean, resultPhrase:string) {
+    const result = num1 + num2;
+
+    if(showResult) {
+        console.log(resultPhrase + result)
+    } else {
+        return result;
+    }
+    return num1 + num2;
+}
+
+let num1: number
+num1 = 1;
+const num2 = 10;
+const printResult = true;
+const resultPhrase = 'Result: '
+
+add(num1, num2, printResult, resultPhrase);
+
